917d29f1fd0e5fedf93eb218fca4301f82828c4f
ridwankechil <70096802+ridwankechil@users.noreply.github.com>
2020-10-17 11:01:57 +0700

    Create Subscribe.sh

---
+++ Subscribe.sh
@@ -0,0 +1,82 @@
+#!/xbin/bash
+#subscribe ya
+clear
+echo $cyan "Subscribe Dulu Ya"
+echo "tunggu sebentar "
+sleep 1
+echo "membuka YouTube"
+sleep 10
+termux-open https://www.youtube.com/channel/UCTMBlJoMW9jaWyiv8oZsjKw
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orange
+ echo $ku "Dukung Channel Gw sampai 1000 Subscriber ya"
+ echo $me "follow Instagram gw biar dapat pemberitahuan jika Bagi² Akun Lagi"
+ echo $pu "Untuk Semuanya> Terimakasih sudah mendukung sy"
+ echo $pu "Menjadi 150 subscriber " $or "jika bisa Tolong jadiin 500 subscriber"
+ echo $me "dan sy usahakan" $or "untuk bagi bagi lebih banyak lagi dari ini"
+ sleep 2
+ termux-open 
+echo $or "
+  ●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
+                 🔴 Jangan Lupa Tersenyum🔴
+  ●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
+  
+═══════════════════════════════════════
+[1] 🔴 Akun Mobile Legends Gratis🔴
+═══════════════════════════════════════
+[2] 🔴 Akun Free Fire Gratis🔴
+═══════════════════════════════════════
+┗────CTRL +c Exit Program"
+echo '\033[35;1m'
+read -p "root@Pilih Nomor > " bro
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+echo $me "Akun Mobilegends Gratis"
+sleep 1
+echo $pu"ini kumpulan akunnya"
+echo $ku "Tunggu ya"
+sleep 1
+echo $cy "~~~~~~~~~~~~~~~~~~~~~~~~"
+sleep 15
+echo $bi "[DIEE] carvertypro@hotmail.com:Legoland13 -> (Limit login)"
+sleep 2
+echo $me "[DIEE] sizzleko@gmail.com:Itoako46 -> (Limit login)s login)"
+sleep 15
+echo $or "[DIEE] kodi.mymrik@gmail.com:Chikarelli2001 -> (Limit login)"
+sleep 20
+echo $pu "[LIVE] arispp21@gmail.com:Aris2121 -> (Success login)"
+sleep 2
+echo $or "[DIEE] grechka2288@gmail.com:Pringleschip2 -> (Limit login)"
+sleep 30
+echo $pu "[LIVE] dab00@bk.ru:Ghbdtn2009 -> (Success login)"
+sleep 1
+echo $or "[DIEE] korzyuk80@gmail.com:Kirill228 -> (Limit login)in)"
+sleep 3
+echo "[DIEE] aymen.bb66@gmail.com:AYBayb007 -> (Limit login)"
+sleep 7
+echo "[DIEE] kodi.mymrik@gmail.com:Chikarelli2001 -> (Limit login)"
+sleep 3
+echo $me "[DIEE] ems6111@gmail.com:Quinska09 -> (Limit login)it login)
+[DIEE] alex.taylor4095@gmail.com:Meltedparking07 -> (Limit login)
+[DIEE] sebyfebriansyah12@gmail.com:Seby2012 -> (Limit login)
+[DIEE] christopherpierce528@gmail.com:Kevin1011 -> (Limit login)"
+sleep 15
+echo "periksa koneksi internet anda"
+sleep 1
+echo "terimakasih sudah berkunjung"
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+echo "maaf Belum di Isi"
+sleep 3
+echo "terimakasih sudah berkunjung"
+fi
